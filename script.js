b0412e201a2c8d0e7bcf4e49415983fa6c957434
Jeff Kersting <jeff.w.kersting@gmail.com>
2020-12-29 21:14:02 -0700

    Add movie villain data

---
+++ script.js
@@ -0,0 +1,5 @@
+var position = document.documentElement;
+position.addEventListener("mousemove", (e) => {
+  position.style.setProperty('--x', e.clientX + 'px')
+  position.style.setProperty('--y', e.clientY + 'px')
+})
