4fed35f0cdd63e8bd6f2b732ccf8a2f826e7bef9
Jeff Kersting <jeff.w.kersting@gmail.com>
2020-12-31 00:01:09 -0700

    Add data and card generation. Add styling

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
+const content = document.querySelector('.content')
+const imgTest = document.querySelector('.test')
+var blood = document.querySelector('.blood')
+document.onload = createCards()
+
 var position = document.documentElement;
 position.addEventListener("mousemove", (e) => {
   position.style.setProperty('--x', e.clientX + 'px')
   position.style.setProperty('--y', e.clientY + 'px')
 })
+
+
+function createCards() {
+  killers.forEach(killer => {
+    content.innerHTML += `
+    <article class="card">
+      <div class="card-corner"></div>
+      <p class="kill-count">Kills <br> ${killer.killCount}</p>
+      <img class="test" src="${killer.image}" alt="${killer.name}"/>
+      <h1 class="name">${killer.name}</h1>
+      <a class="wiki-page" href=""></a>
+      <h2 class="ratings">Highest Rated Film</h2>
+      <a class="rotten-tomatoes"></a>
+      <h2 class="films">No. Films: ${killer.totalFilms}</h2>
+      <a class="appearances"></a>
+    </article>
+    `
+  })
+}
